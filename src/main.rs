2679f7857c89697398f327fdc3c99e8a6ac2b731
Craig Furman <craig.furman89@gmail.com>
2017-09-11 19:03:32 +0100

    Can pass stderr/stdin to lib for integration tests

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,5 @@ use std::io;
 fn main() {
     let argv: Vec<String> = env::args().collect();
     let vars = env::vars().collect();
-    container_funtime::run(argv, vars, io::stdout());
+    container_funtime::run(argv, vars, io::stdin(), io::stdout(), io::stderr());
 }
