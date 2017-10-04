d2dab73accaf046c6453dbedc8976bfc5ac84d0a
Craig Furman <craig.furman89@gmail.com>
2017-10-04 18:06:22 +0100

    Remove almost everything

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # container_funtime
 
 This is very work-in-progress, in fact this is not a container runtime at all
-yet. It is only a demo of passing Read/Write implementors for stdio to make
-integration testing easier without compiling main.
+yet.
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,1 @@
-extern crate container_funtime;
-
-use std::env;
-use std::io;
-
-fn main() {
-    let argv: Vec<String> = env::args().collect();
-    let vars = env::vars().collect();
-    container_funtime::run(argv, vars, io::stdin(), io::stdout(), io::stderr());
-}
+fn main() {}
