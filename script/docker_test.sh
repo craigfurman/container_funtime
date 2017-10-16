6f8de5754882e382e68905174969554752856bcd
Craig Furman <cfurman@pivotal.io>
2017-10-16 17:41:23 +0100

    Pass alpine rootfs into docker tests

--- script/docker_test.sh
+++ script/docker_test.sh
@@ -2,5 +2,7 @@
 set -euo pipefail
 
 project=$(cd "$(dirname "$0")"/.. && pwd)
-docker run --privileged --rm -u dave:dave -v "$project":/tests -w /tests \
+docker run --privileged --rm -u dave:dave -w /tests \
+  -v "$project":/tests -v "$CTR_TESTS_ALPINE_ROOTFS":/rootfs/alpine \
+  -e CTR_TESTS_ALPINE_ROOTFS=/rootfs/alpine \
   craigfurman/container-funtime-testing /tests/script/test_in_container.sh
